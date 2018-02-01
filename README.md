312f7f17004afc979118b3a911e7119ec9eb4fe7
Dandy Martin <dandysrt@outlook.com>
2018-01-31 20:19:51 -0500

    add comments, update README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # url_shortener
 pythonic URL shortening service
+
+## Features
+* Importable Class-based structure
+* Abstract class for 'API' guidance
+
+## Quick Usage Guide
+1. Import the shorten.py URLSHORTENER class
+```python
+    from shorten import URLSHORTENER
+```
+2. Initialize URLSHORTENER with your host, table size, and url-object time-to-live values
+```python
+    myshortener = URLSHORTENER('www.myhost.com', 999, 3)
+```
+3. Generate a shortened URL with a call to get_short_url function, passing the long_url. If your host is http unsecured, set the second optional value to True
+```python
+    short_url = myshortener.get_short_url('www.somehost.net/there/is/a/whole/bunch/of?what') # if secured
+    us_short_url = myshortener.get_short_url('www.somehost.net/there/is/a/whole/bunch/of?what', http=True)  # if unsecured
+```
+4. Similarly, in order to retrieve the long-version of your url, simply call get_long_url with the shortened url value
+```python
+    long_url = myshortener.get_long_url(short_url)
+```
+
+### Requirements
+build requires python stdlib imports: struct, random, and datetime
+#### Special requirements are a custom urltable classfile with a URLTABLE class object though a default has been provided
--- shorten.py
+++ shorten.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-import os, sys
 import random
 import struct
 from urltable import URLTABLE
@@ -8,15 +7,27 @@ from urltable import URLTABLE
 
 class URLSHORTENER(object):
     '''
-     @Desc:
+     @ClassDesc: Class for shortening long URLs
     '''
 
     def __init__(self, host, size, delta=1):
+        '''
+         @Desc: class constructor
+         @Params: host - hostname for shortened URL,
+          size - size of URLTABLE object,
+          delta - time in hours for URLOBJECT Time To Live(TTL)
+        '''
         self.host = host
         self.urltable = URLTABLE(size, delta)
 
 
     def build_key(self, seed=None):
+        '''
+         @Desc: function to build URLTABLE key
+         @Params: seed - optional seed value
+         @Return: i_key - integer representation of key,
+           s_key - string representation of key
+        '''
         klist = ['>5B']
         i_key = 0
         random.seed(seed)
@@ -26,7 +37,7 @@ class URLSHORTENER(object):
             mult = random.randint(1, 2)     # ascii a-z is nearly double 0-9 values
             add = 0
             if mult > 1:
-                add = random.randint(1, 8)
+                add = random.randint(1, 8)  # lower to upper range of values for a-z
             asc = (base * mult) + add
             klist.append(asc)
             i_key+= asc
@@ -34,7 +45,13 @@ class URLSHORTENER(object):
         return i_key, s_key
 
     def build_url(self, host, string_key, http=False):
-
+        '''
+         @Desc: function to build shortened url
+         @Params: host - short url hostname,
+           string_key - generated string key,
+           http - host is http unsecured(True/False)
+         @Return: shortened url
+        '''
         header = 'https://{0}'.format(host)
         if http:
             header = 'http://{0}'.format(host)
@@ -44,12 +61,30 @@ class URLSHORTENER(object):
 
 
     def push_to_table(self, int_key, string_key, long_url):
-        self.urltable.push(int_key, string_key, long_url)
+        '''
+         @Desc: function to push long_url to URLTABLE object
+         @Params: int_key - generated integer key,
+           string_key - generated string key,
+           long_url - long url
+         @Return: URLTABLE push function return value
+        '''
+        return self.urltable.push(int_key, string_key, long_url)
 
     def get_long_url(self, short_url):
+        '''
+         @Desc: function to retrieve long_url from URLTABLE object
+         @Params: short_url - shortened url
+         @Return: long url
+        '''
         return self.urltable.retrieve(short_url.split('/')[-1]) # split and grab key from end of url
 
     def get_short_url(self, long_url, http=False):
+        '''
+         @Desc: function to generate shortened url, and
+           push generated URLOBJECT to URLTABLE object
+         @Params: long_url - long url, http - host is unsecured(True/False)
+         @Return: shortened url
+        '''
         i_key, s_key = self.build_key(long_url)
         self.push_to_table(i_key, s_key, long_url)
         return self.build_url(self.host, s_key, http)
